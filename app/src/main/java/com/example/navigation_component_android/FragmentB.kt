542c2745491b8c7959541f0b06cb67400491fb86
germanheinz <heinz.german@gmail.com>
2020-06-02 17:18:20 +0200

    Adding ActionBar

--- app/src/main/java/com/example/navigation_component_android/FragmentB.kt
+++ app/src/main/java/com/example/navigation_component_android/FragmentB.kt
@@ -17,7 +17,11 @@ import kotlinx.android.synthetic.main.fragment_b.*
 class FragmentB : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
-        return inflater.inflate(R.layout.fragment_b, container, false) }
+
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/navigation_component_android/MainActivity.kt
+++ app/src/main/java/com/example/navigation_component_android/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.navigation_component_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
